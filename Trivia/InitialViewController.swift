bf04806dfa4d7e2d16efc00afac39fde074ca82b
bastiampos <bastiampos.job@gmail.com>
2022-03-18 14:10:42 -0300

    autolayout desde el builder sin codigo

---
+++ Trivia/InitialViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InitialViewController.swift
+//  Trivia
+//
+//  Created by Sebastian Campos on 18/03/2022.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    @IBOutlet weak var userTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
